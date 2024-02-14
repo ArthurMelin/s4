a2a5c96e5b40288ac3cb9c353c2d34a51d9c90ff
Arthur Melin <elassyo@gmail.com>
2024-02-14 21:03:22 +0100

    bump version
    didn't forget this time :)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name="s4",
-   version="0.1.0",
+   version="0.1.2",
    author="Arthur Melin",
    url="https://github.com/ArthurMelin/s4",
    python_requires=">=3.12",
